92022a1b74a03ea30a97a8a7018cbcdc5a87766a
Ganesh Pandey <pandey.ganesh20@yahoo.com>
2021-11-26 18:57:52 -0600

    Initial commit

---
+++ src/test/java/patch/practise/PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package patch.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
